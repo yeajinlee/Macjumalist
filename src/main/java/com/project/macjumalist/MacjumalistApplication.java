62217e9f5381b18916819ca82a83fe94665c51e5
Yeajin Lee <84839372+yeajinlee@users.noreply.github.com>
2022-11-30 15:39:53 +0900

    first commit

---
+++ src/main/java/com/project/macjumalist/MacjumalistApplication.java
@@ -0,0 +1,13 @@
+package com.project.macjumalist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacjumalistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacjumalistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/macjumalist/MacjumalistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.macjumalist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacjumalistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
